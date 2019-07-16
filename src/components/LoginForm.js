22d12a92b39c8bb8885794d0296f5e44a4ee33c9
chetchopra <chetanya.chopra94@gmail.com>
2019-07-16 13:38:23 -0700

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,40 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {username: "",
+  password: ""};
+  }
+
+  handleUsername = (event) => {
+    let newInput = event.target.value;
+    this.setState({username: newInput})
+  }
+
+  handlePassword = (event) => {
+    let newInput = event.target.value;
+    this.setState({password: newInput})
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.username !== "" || this.state.password !== "")
+    if (!this.state.username || !this.state.password) return
+    this.props.handleLogin(this.state)
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleOnSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleUsername}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handlePassword}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,25 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {value: "",
+    charsLeft: this.props.maxChars}
+  }
+
+  handleOnChange = (event) => {
+    let newInput = event.target.value;
+    this.setState({value: newInput})
+    this.setState({charsLeft: this.state.charsLeft - 1}) 
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleOnChange} maxLength={this.props.maxChars} value={this.state.value}/>
+        <p>{this.state.charsLeft} characters remaining</p>
       </div>
     );
   }
