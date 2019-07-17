becf51d47c16745fe61320e1ed314e7ff16d3f01
fut0k0 <cccabell@icloud.com>
2019-07-17 10:37:45 -0700

    Passing lab

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import LoginForm from "./components/LoginForm";
 import TwitterMessage from "./components/TwitterMessage";
 
 class App extends Component {
 
-  login = ({ username, password }) => {
-    console.log(`Logging in ${username} with password ${password}`);
-  };
+	login = ({ username, password }) => {
+		console.log(`Logging in ${username} with password ${password}`);
+	};
 
-  render() {
-    return (
-      <div>
+	render() {
+		return (
+			<div>
 
-        <h1>
-          <pre>LoginForm</pre>
-        </h1>
-        <LoginForm handleLogin={this.login} />
+				<h1>
+					<pre>LoginForm</pre>
+				</h1>
+				<LoginForm handleLogin={this.login} />
 
-        <h1>
-          <pre>TwitterMessage</pre>
-        </h1>
-        <TwitterMessage maxChars={140} />
+				<h1>
+					<pre>TwitterMessage</pre>
+				</h1>
+				<TwitterMessage maxChars={140} />
 
 
 
-      </div>
-    )
-  }
+			</div>
+		)
+	}
 }
 
-export default App
+export default App;
